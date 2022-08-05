548161b98ff767f98bbf3a56acab72841827c91b
Tuan Nguyen Anh <tuan.na220891@gmail.com>
2022-08-06 08:25:56 +0900

    receive, transfer, and send funds

--- contracts/ExerciseC6B.sol
+++ contracts/ExerciseC6B.sol
@@ -10,6 +10,23 @@ import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract ExerciseC6B {
     using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
 
+    uint256 private enabled = block.timestamp;
+
+    uint256 private counter = 1; 
+
+    modifier rateLimit(uint time) {
+        require(block.timestamp >= enabled, "Rate limiting in effect");
+        enabled = enabled.add(time);
+        _;
+    }
+
+    modifier entrancyGuard() {
+        counter = counter.add(1);
+        uint256 guard = counter;
+        _;
+
+        require(guard == counter, "That is not allowd");
+    }
     /********************************************************************************************/
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
